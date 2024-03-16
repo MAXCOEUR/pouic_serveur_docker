0b0fe93b242e23549c046ce81d5c24560630a6f9
Maxence COEUR <maxence.chachane@gmail.com>
2024-03-16 15:25:30 +0100

    update sql

--- sqldb/Dump20230829.sql
+++ sqldb/Dump20230829.sql
@@ -6,43 +6,42 @@
 
 ALTER SCHEMA `pouic`  DEFAULT COLLATE utf8mb4_unicode_ci;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
--- Table structure for table `amis`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `amis`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `amis` (
-  `demandeur` varchar(80) NOT NULL,
-  `receveur` varchar(80) NOT NULL,
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `email` varchar(255) NOT NULL,
+  `uniquePseudo` varchar(80) NOT NULL,
+  `pseudo` varchar(80) NOT NULL,
+  `passWord` varchar(255) NOT NULL,
+  `extension` varchar(4) DEFAULT NULL,
+  `bio` varchar(200) DEFAULT NULL,
+  `tokenFireBase` varchar(170) DEFAULT NULL,
+  PRIMARY KEY (`uniquePseudo`),
+  UNIQUE KEY `uniquePseudo_UNIQUE` (`uniquePseudo`),
+  UNIQUE KEY `email_UNIQUE` (`email`)
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `file`
+--
+
+DROP TABLE IF EXISTS `file`;
+CREATE TABLE `file` (
   `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `id_message` int(11) unsigned DEFAULT NULL,
+  `linkFile` varchar(255) DEFAULT NULL,
+  `name` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `amis-user_idx` (`demandeur`,`receveur`),
-  KEY `tt_idx` (`receveur`),
-  CONSTRAINT `amis-demandeur->user` FOREIGN KEY (`demandeur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `amis-receveur->user` FOREIGN KEY (`receveur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+  KEY `file->message_idx` (`id_message`)
+) ENGINE=InnoDB ;
 --
 -- Table structure for table `conversation`
 --
 
 DROP TABLE IF EXISTS `conversation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `conversation` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(255) DEFAULT NULL,
@@ -51,69 +50,12 @@ CREATE TABLE `conversation` (
   PRIMARY KEY (`id`),
   KEY `conversation->user_idx` (`uniquePseudo_admin`),
   CONSTRAINT `conversation->user` FOREIGN KEY (`uniquePseudo_admin`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `demandeAmis`
---
-
-DROP TABLE IF EXISTS `demandeAmis`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `demandeAmis` (
-  `demandeur` varchar(80) NOT NULL,
-  `receveur` varchar(80) NOT NULL,
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`),
-  KEY `amis-user_idx` (`demandeur`,`receveur`),
-  KEY `tt_idx` (`receveur`),
-  CONSTRAINT `demandeAmis-demandeur->user` FOREIGN KEY (`demandeur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE,
-  CONSTRAINT `demandeAmis-receveur->user` FOREIGN KEY (`receveur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `file`
---
-
-DROP TABLE IF EXISTS `file`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `file` (
-  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `id_message` int(11) unsigned DEFAULT NULL,
-  `linkFile` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `file->message_idx` (`id_message`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `message-read`
---
-
-DROP TABLE IF EXISTS `message-read`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `message-read` (
-  `id_message` int(11) unsigned NOT NULL,
-  `uniquePseudo_user` varchar(80) NOT NULL,
-  PRIMARY KEY (`id_message`,`uniquePseudo_user`),
-  KEY `message-read->user` (`uniquePseudo_user`),
-  CONSTRAINT `message->message` FOREIGN KEY (`id_message`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
-  CONSTRAINT `message-read->user` FOREIGN KEY (`uniquePseudo_user`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE=InnoDB;
 --
 -- Table structure for table `messages`
 --
 
 DROP TABLE IF EXISTS `messages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `messages` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `uniquePseudo_sender` varchar(80) NOT NULL,
@@ -128,16 +70,61 @@ CREATE TABLE `messages` (
   CONSTRAINT `message->conv` FOREIGN KEY (`id_conversation`) REFERENCES `conversation` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `message->messageParent` FOREIGN KEY (`id_parent`) REFERENCES `messages` (`id`) ON DELETE SET NULL ON UPDATE CASCADE,
   CONSTRAINT `message->user` FOREIGN KEY (`uniquePseudo_sender`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB ;
+--
+-- Table structure for table `amis`
+--
+
+DROP TABLE IF EXISTS `amis`;
+CREATE TABLE `amis` (
+  `demandeur` varchar(80) NOT NULL,
+  `receveur` varchar(80) NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `amis-user_idx` (`demandeur`,`receveur`),
+  KEY `tt_idx` (`receveur`),
+  CONSTRAINT `amis-demandeur->user` FOREIGN KEY (`demandeur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `amis-receveur->user` FOREIGN KEY (`receveur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+
+--
+-- Table structure for table `demandeAmis`
+--
+
+DROP TABLE IF EXISTS `demandeAmis`;
+CREATE TABLE `demandeAmis` (
+  `demandeur` varchar(80) NOT NULL,
+  `receveur` varchar(80) NOT NULL,
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`),
+  KEY `amis-user_idx` (`demandeur`,`receveur`),
+  KEY `tt_idx` (`receveur`),
+  CONSTRAINT `demandeAmis-demandeur->user` FOREIGN KEY (`demandeur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `demandeAmis-receveur->user` FOREIGN KEY (`receveur`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB;
+
+--
+-- Table structure for table `message-read`
+--
+
+DROP TABLE IF EXISTS `message-read`;
+CREATE TABLE `message-read` (
+  `id_message` int(11) unsigned NOT NULL,
+  `uniquePseudo_user` varchar(80) NOT NULL,
+  PRIMARY KEY (`id_message`,`uniquePseudo_user`),
+  KEY `message-read->user` (`uniquePseudo_user`),
+  CONSTRAINT `message->message` FOREIGN KEY (`id_message`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
+  CONSTRAINT `message-read->user` FOREIGN KEY (`uniquePseudo_user`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
+
+
 
 --
 -- Table structure for table `reactions`
 --
 
 DROP TABLE IF EXISTS `reactions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reactions` (
   `message_id` int(11) unsigned NOT NULL,
   `user_uniquePseudo` varchar(80) NOT NULL,
@@ -147,36 +134,12 @@ CREATE TABLE `reactions` (
   CONSTRAINT `reaction->messages` FOREIGN KEY (`message_id`) REFERENCES `messages` (`id`) ON DELETE CASCADE ON UPDATE NO ACTION,
   CONSTRAINT `reaction->user` FOREIGN KEY (`user_uniquePseudo`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Table structure for table `user`
---
-
-DROP TABLE IF EXISTS `user`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `user` (
-  `email` varchar(255) NOT NULL,
-  `uniquePseudo` varchar(80) NOT NULL,
-  `pseudo` varchar(80) NOT NULL,
-  `passWord` varchar(255) NOT NULL,
-  `extension` varchar(4) DEFAULT NULL,
-  `bio` varchar(200) DEFAULT NULL,
-  `tokenFireBase` varchar(170) DEFAULT NULL,
-  PRIMARY KEY (`uniquePseudo`),
-  UNIQUE KEY `uniquePseudo_UNIQUE` (`uniquePseudo`),
-  UNIQUE KEY `email_UNIQUE` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Table structure for table `user-conversation`
 --
 
 DROP TABLE IF EXISTS `user-conversation`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `user-conversation` (
   `uniquePseudo_user` varchar(80) NOT NULL,
   `id_conversation` int(11) unsigned NOT NULL,
@@ -186,7 +149,6 @@ CREATE TABLE `user-conversation` (
   CONSTRAINT `user->user-conversation` FOREIGN KEY (`uniquePseudo_user`) REFERENCES `user` (`uniquePseudo`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `user-conversation->conversation` FOREIGN KEY (`id_conversation`) REFERENCES `conversation` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping routines for database 'discution'
@@ -732,19 +694,7 @@ begin
 select * from reactions r join user u on r.user_uniquePseudo=u.uniquePseudo where r.message_id=message_id;
 end ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `MarkAllUnreadMessagesAsRead` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8mb4 */ ;
-/*!50003 SET character_set_results = utf8mb4 */ ;
-/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`maxence`@`%` PROCEDURE `MarkAllUnreadMessagesAsRead`(
   IN conversationId INT UNSIGNED,
@@ -814,18 +764,5 @@ BEGIN
     WHERE reactions.message_id = p_message_id AND reactions.user_uniquePseudo = p_user_uniquePseudo;
 END ;;
 DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2023-09-17 13:09:45
