b381b98915333b65e56f85c4fc06a4c981c14d1e
Maxence COEUR <maxence.chachane@gmail.com>
2024-04-14 19:30:52 +0200

    re

--- apiNode/FireBaseNotification.js
+++ apiNode/FireBaseNotification.js
@@ -11,16 +11,15 @@ function sendNotif(title,body,token) {
     notification: {
       title: title,
       body: body,
-      // Ajoutez ces paramètres pour spécifier le comportement de la notification
-      android: {
-        priority: 'high',
-        notification: {
-          sound: 'default',
-          vibrate: 'true'
-        }
-      },
     },
     token: token,
+    android: {
+      priority: 'high',
+      notification: {
+        sound: 'default',
+        vibrate: 'true'
+      }
+    }
   };
 
   firebase
